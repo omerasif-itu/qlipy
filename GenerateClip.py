114efd9153fa8a80e4dcc487dff605d6eb28b47f
Omer Asif <mscs16039@itu.edu.pk>
2020-09-12 23:54:24 +0500

    refactoring...

--- GenerateClip.py
+++ GenerateClip.py
@@ -5,7 +5,7 @@ from moviepy.editor import *
 
 # ------------------------------
 # directory structure should be:
-# --- combine/
+# --- qlipy/
 # ----- GenerateClip.py
 # ----- videos/
 # ----- audios/
